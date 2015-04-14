b9bc862c3ab80e0dfc82587664ef92667fc3158f
Wayne Campbell <w-campbell.2@onu.edu>
2015-04-14 10:39:09 -0400

    Works for the first time now

--- src/mp5package/Cannon.java
+++ src/mp5package/Cannon.java
@@ -11,4 +11,20 @@ package mp5package;
  */
 public class Cannon {
     int x,y;
+    
+    public int getX(){
+        return x;
+    }
+    
+    public void setX(int X){
+        this.x = X;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setY(int Y){
+        this.y = Y;
+    }
 }
